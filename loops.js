15134644f873189cbb8fca988daa72cc7cb568f2
Anastasiia <solnce.more.oblaka@gmail.com>
2023-08-30 20:11:14 -0400

    added_based_conditionelse_clause

--- loops.js
+++ loops.js
@@ -3,3 +3,14 @@ let num215 = 215
 if (num215 == 215) {
     console.log('yes, value under condition')
 }
+
+if (num215 >= 215 && num215 < 250) {
+    console.log('Second condition works')
+}
+
+if (num215 > 300) {
+    console.log('num215 varaible greater than 300')
+}
+else {
+    console.log('code goes to the else')
+}
