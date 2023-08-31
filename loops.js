0b94d605aeb637737b06e007db625f54afd80bc3
Anastasiia <solnce.more.oblaka@gmail.com>
2023-08-30 20:24:10 -0400

    added_multiple_if_else

--- loops.js
+++ loops.js
@@ -11,6 +11,15 @@ if (num215 >= 215 && num215 < 250) {
 if (num215 > 300) {
     console.log('num215 varaible greater than 300')
 }
+
+else if (num215 >=250) {
+    console.log('num greater than 200')
+}
+
+else if (num215 >=200) {
+    console.log('num greater than 200')
+}
+
 else {
     console.log('code goes to the else')
 }
