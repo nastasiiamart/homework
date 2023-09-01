c6735cc81bf9140e9ccd4233a0099af9e942107a
Anastasiia <solnce.more.oblaka@gmail.com>
2023-09-01 13:13:58 -0400

    added_while_DO

--- loops.js
+++ loops.js
@@ -1,29 +1,40 @@
-let num215 = 215
+// let num215 = 215
 
-if (num215 == 215) {
-    console.log('yes, value under condition')
-}
+// if (num215 == 215) {
+//     console.log('yes, value under condition')
+// }
 
-if (num215 >= 215 && num215 < 250) {
-    console.log('Second condition works')
-}
+// if (num215 >= 215 && num215 < 250) {
+//     console.log('Second condition works')
+// }
 
-if (num215 > 300) {
-    console.log('num215 varaible greater than 300')
-}
+// if (num215 > 300) {
+//     console.log('num215 varaible greater than 300')
+// }
 
-else if (num215 >=250) {
-    console.log('num greater than 200')
-}
+// else if (num215 >=250) {
+//     console.log('num greater than 200')
+// }
 
-else if (num215 >=200) {
-    console.log('num greater than 200')
-}
+// else if (num215 >=200) {
+//     console.log('num greater than 200')
+// }
 
-else {
-    console.log('code goes to the else')
-}
+// else {
+//     console.log('code goes to the else')
+// }
 
-            (num215 >= 200) ? console.log('ternary true') : 
-            (num215 >= 250) ? console.log('ternary false') :
-            console.log('ternary false')
+//             (num215 >= 200) ? console.log('ternary true') : 
+//             (num215 >= 250) ? console.log('ternary false') :
+//             console.log('ternary false')
+
+let num = 10;
+// while (num <= 7) {
+//     console.log(num)
+//     num++;
+// }
+
+// do {
+//     console.log(num)
+//     num++
+// } while (num <= 10)
